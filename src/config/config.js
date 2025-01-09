b0dd0ac3d4cffaab2d5e118a81a968b973795568
Kairat Tobokelov <kajrattobokelov2@gmail.com>
2025-01-09 12:01:47 +0600

    changed config

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,11 @@
 const bbox = [-180.0, -90.0, 180.0, 90.0];
 const maxHeight = 20;
-const blockSizeMeters = 100;
+const blockSizeMeters = 10;
 const blockHalfSize = blockSizeMeters / 2;
 const offsetY = 0.0;
 
 const seed = 12345;
-const chunkSize = 16;
-const renderDistance = 2;
+const chunkSize = 2;
+const renderDistance = 4;
 
 const metersPerLatDegree = 111_320;
