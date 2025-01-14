44405e8b9488aeeb8be91e24af080adefd221379
Kairat Tobokelov <kajrattobokelov2@gmail.com>
2025-01-14 17:24:51 +0900

    added blocks hiding mechanism after it is out of vision distance

--- src/world/world.js
+++ src/world/world.js
@@ -7,13 +7,13 @@ class World {
         this.layerList = new Module.JSLayerList(true);
         this.layer = this.layerList.createLayer('world', Module.ELT_POLYHEDRON);
         this.layer.setMinDistance(0);
-        this.layer.setMaxDistance(1000000);
+        this.layer.setMaxDistance(130);
     }
 
     loadChunk(playerX, playerZ) {
         const chunkX = Math.floor(playerX / this.chunkSize);
         const chunkZ = Math.floor(playerZ / this.chunkSize);
-    
+
         const startChunkX = chunkX - this.renderDistance;
         const endChunkX = chunkX + this.renderDistance;
         const startChunkZ = chunkZ - this.renderDistance;
@@ -25,6 +25,7 @@ class World {
     
                 if (!this.loadedChunks.has(chunkKey)) {
                     const newChunk = new Chunk(x, z, this.chunkSize, this.seed);
+                    this.loadedChunks.set(chunkKey, newChunk);
 
                     if (this.spawn === null || this.spawn === undefined) {
                         this.spawn = newChunk.blocks[0].getCorners()[0];
@@ -54,12 +55,8 @@ class World {
                         }
 
                         polygon.setStyle(polygonStyle);
-
                         this.layer.addObject(polygon, 0);
                     });
-                    
-                    this.loadedChunks.set(chunkKey, newChunk);
-                    console.log(`Loaded chunk at (${x}, ${z}) with seed: ${this.seed}`);
                 }
             }
         }
@@ -72,11 +69,11 @@ class World {
         for (let [key, chunk] of this.loadedChunks) {
             const [chunkX, chunkZ] = key.split(',').map(Number);
     
-            const distance = Math.abs(chunkX - playerChunkX) + Math.abs(chunkZ - playerChunkZ);
+            const distanceX = Math.abs(chunkX - playerChunkX);
+            const distanceZ = Math.abs(chunkZ - playerChunkZ);
     
-            if (distance > this.renderDistance) {
+            if (distanceX > this.renderDistance || distanceZ > this.renderDistance) {
                 this.loadedChunks.delete(key);
-                console.log(`Unloaded chunk at (${chunkX}, ${chunkZ})`);
             }
         }
     }
@@ -85,6 +82,6 @@ class World {
         var playerX = player.position.x;
         var playerZ = player.position.z;
         this.loadChunk(playerX, playerZ);
-        this.unloadChunk(playerX, playerZ);
+        // this.unloadChunk(playerX, playerZ);
     }
 }
